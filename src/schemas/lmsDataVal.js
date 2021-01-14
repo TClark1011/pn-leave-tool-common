3f5a21c901c728b565734b966633a2861a6aec70
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 10:56:10 +1100

    refactored lms data validation

--- src/schemas/lmsDataVal.js
+++ src/schemas/lmsDataVal.js
@@ -1,14 +1,13 @@
 import { object, string, array } from "yup";
 
-export const basesLmsDataValSchema = {
-	file: array()
-		.of(
-			object({
-				Name: string().required("Invalid CSV file"),
-			}),
-		)
-		.required("Please upload a file"),
-	depot: string().required("Please select a depot"),
-};
+export const fileField = array()
+	.of(
+		object({
+			Name: string().required("Invalid CSV file"),
+		}),
+	)
+	.required("Please upload a file");
 
-export default object(basesLmsDataValSchema);
+export const depotField = string().required("Please select a depot");
+
+export default object({ file: fileField, depot: depotField });
--- src/schemas/lmsDataValFrontend.js
+++ src/schemas/lmsDataValFrontend.js
@@ -1,7 +1,8 @@
-import { basesLmsDataValSchema } from "./lmsDataVal";
+import { fileField, depotField } from "./lmsDataVal";
 import { object, string } from "yup";
 
 export default object({
-	...basesLmsDataValSchema,
+	file: fileField,
+	depot: depotField,
 	access_key: string().required("Please enter an access key"),
 });
