9962fb496ef93ff923cee8f44ef6460045f581bb
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 10:28:46 +1100

    refactored required conditions

--- src/schemas/loginVal.js
+++ src/schemas/loginVal.js
@@ -2,6 +2,8 @@ import { object } from "yup";
 import { employeeNumberVal, passwordVal } from "../commonAuthFields";
 
 export default object({
-	"employee_number": employeeNumberVal,
-	"password": passwordVal,
+	employee_number: employeeNumberVal.required(
+		"You must enter your employee number",
+	),
+	password: passwordVal.required("You must enter your password"),
 });
