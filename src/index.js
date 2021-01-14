23f024d3e805ebaa520fd1b89cb3c12fc2a57136
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 10:30:49 +1100

    export new validation from index

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import leaveVal from "./schemas/leaveVal";
 import loginVal from "./schemas/loginVal";
 import lmsDataVal from "./schemas/lmsDataVal";
 import registerVal from "./schemas/registerVal";
+import resetPasswordVal from "./schemas/resetPasswordVal";
+import forgotPasswordVal from "./schemas/forgotPasswordVal";
 import emailRegex from "./regex/emailRegex";
-export { leaveVal, loginVal, lmsDataVal, registerVal, emailRegex };
+export {
+	leaveVal,
+	loginVal,
+	lmsDataVal,
+	registerVal,
+	emailRegex,
+	forgotPasswordVal,
+	resetPasswordVal,
+};
