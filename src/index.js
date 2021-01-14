efa83ed2b9dd26911919ec59ba832ef3ad7e9a18
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 09:38:06 +1100

    import/export all files in index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Hello from src/index.js");
+import leaveVal from "./schemas/leaveVal";
+import loginVal from "./schemas/loginVal";
+import lmsDataVal from "./schemas/lmsDataVal";
+import registerVal from "./schemas/registerVal";
+import emailRegex from "./regex/emailRegex";
+export { leaveVal, loginVal, lmsDataVal, registerVal, emailRegex };
