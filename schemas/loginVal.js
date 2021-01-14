72348fb7346a0344c06831b3b87f3452e694f5e4
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 09:11:10 +1100

    initial setup

--- schemas/loginVal.js
+++
@@ -1,9 +0,0 @@
-import { object } from "yup";
-import { employeeNumberVal, passwordVal } from "./commonAuthFields";
-
-export default object({
-	"employee_number": employeeNumberVal.required(
-		"Employee Number is a required field"
-	),
-	"password": passwordVal.required("Password is a required field"),
-});
