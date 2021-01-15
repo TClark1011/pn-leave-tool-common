e63248c198555352fcccb8f21d13a3d8d8ab52e7
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 11:25:35 +1100

    added profile validation

---
+++ src/schemas/profileVal.js
@@ -0,0 +1,20 @@
+import { string, object } from "yup";
+
+/**
+ * Generates profile validation
+ *
+ * @param {string[]} depots The list of valid depots
+ * @returns {yup.schema} The yup schema that can be used to validate profile edits
+ */
+const getProfileVal = (depots) =>
+	object({
+		name: string().required("Name cannot be blank"),
+		depot: string()
+			.oneOf(
+				depots.map((item) => item._id),
+				`Selected depot is invalid`,
+			)
+			.required("You must select a depot"),
+	});
+
+export default getProfileVal;
