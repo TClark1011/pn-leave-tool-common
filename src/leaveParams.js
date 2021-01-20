7b3326f9638efe8e8040e1b66d5e9f6e41c14f28
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-21 09:10:58 +1100

    created leaveParams file

---
+++ src/leaveParams.js
@@ -0,0 +1,12 @@
+/**
+ * The minimum number of days (from the current date) that leave can start
+ */
+export const leaveStartMinOffset = 14;
+
+/**
+ * Minimum/maximum length (in days) of annual leave
+ */
+export const leaveLength = {
+	"min": 1,
+	"max": 100,
+};
