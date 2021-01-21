88cf0edc7a577bd278e75c6232fdd17568036691
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-22 10:59:34 +1100

    added extra version of registration validation for testing email sending

--- src/validation/schemas/registerVal.js
+++ src/validation/schemas/registerVal.js
@@ -2,28 +2,42 @@ import { object, ref, string } from "yup";
 import { employeeNumberVal, passwordVal } from "../commonAuthFields";
 import emailRegex from "../regex/emailRegex";
 
-export default object({
-	"employee_number": employeeNumberVal.required(
-		"You must enter your employee number"
-	),
-	"confirm_employee_number": employeeNumberVal
-		.required("You must confirm your Employee Number")
-		.test(
-			"Employee Numbers match",
-			"Employee Numbers do not match",
-			function (value) {
-				return value ? value === this.resolve(ref("employee_number")) : true;
-			}
+/**
+ * Generate registration validation.
+ *
+ * @param {boolean} [isTesting=false] If it is being used in a testing environment. If true, email will accept any string.
+ * @returns {yup.object} The yup validation object
+ */
+const getRegisterVal = (isTesting = false) =>
+	object({
+		"employee_number": employeeNumberVal.required(
+			"You must enter your employee number"
 		),
-	"password": passwordVal.required("You must enter a password"),
-	"confirm_password": string()
-		.required("You must confirm your password")
-		.test("Passwords match", "Passwords do not match", function (value) {
-			return value ? value === this.resolve(ref("password")) : true;
-		}),
-	"name": string().required("Please enter your name"),
-	"depot": string().required("You must select your depot"),
-	"email": string()
-		.required("Please enter a valid email address")
-		.matches(emailRegex, "Must use a valid Pacific National email address"),
-});
+		"confirm_employee_number": employeeNumberVal
+			.required("You must confirm your Employee Number")
+			.test(
+				"Employee Numbers match",
+				"Employee Numbers do not match",
+				function (value) {
+					return value ? value === this.resolve(ref("employee_number")) : true;
+				}
+			),
+		"password": passwordVal.required("You must enter a password"),
+		"confirm_password": string()
+			.required("You must confirm your password")
+			.test("Passwords match", "Passwords do not match", function (value) {
+				return value ? value === this.resolve(ref("password")) : true;
+			}),
+		"name": string().required("Please enter your name"),
+		"depot": string().required("You must select your depot"),
+		"email": string()
+			.required("Please enter a valid email address")
+			.matches(
+				isTesting ? /.*/ : emailRegex,
+				"Must use a valid Pacific National email address"
+			),
+	});
+
+export default getRegisterVal();
+
+export const testingRegisterVal = getRegisterVal(true);
