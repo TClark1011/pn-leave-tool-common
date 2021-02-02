9b80b6a1cb49a67fe05f8c662f10745abba4537b
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-02 11:37:20 +1100

    leave validation checks that leave meets min/max leave length

--- src/validation/schemas/leaveVal.js
+++ src/validation/schemas/leaveVal.js
@@ -1,5 +1,6 @@
-import { startOfToday, addWeeks } from "date-fns";
+import { startOfToday, addWeeks,differenceInDays } from "date-fns";
 import { object, date, ref } from "yup";
+import { leaveLength } from "../../leaveParams";
 import { employeeNumberVal } from "../commonAuthFields";
 
 export default object({
@@ -14,6 +15,11 @@ export default object({
 				function (value) {
 					return new Date(value) > new Date(this.resolve(ref("start")));
 				}
-			),
+			).test("length within maximum","Leave length must be within maximum", function(value) {
+				return differenceInDays(new Date(value),new Date(this.resolve(ref("start"))) ) <= leaveLength.max;
+			}),
+			).test("length meets minimum","Leave length must be equal to or above minimum", function(value) {
+				return differenceInDays(new Date(value),new Date(this.resolve(ref("start"))) ) >= leaveLength.min;
+			}),
 	}),
 });
